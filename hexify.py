12c8051b1c9835d5e1875ab918e433f803685420
Anonymous <c83081f5@bVtzKmXyOp.com>
2025-03-13 14:38:19 +1000

    improve a few things

--- hexify.py
+++ hexify.py
@@ -1,8 +1,7 @@
 import re
 
-def hexify(s):
+def hexify(string, min_length = 3, colors=False):
     def rpl(match):
-        prefix = match.group(1)
         r = match.group(2)
         r = r.replace("g","6")
         r = r.replace("i","1")
@@ -11,25 +10,13 @@ def hexify(s):
         r = r.replace("s","5")
         r = r.replace("t","7")
         r = r.replace("z","2")
-        r = "0x" + r.upper()
-        return prefix + r
-    return re.sub("(^|\\s)([0-9abcdefgilostz]+)(?=$|\\s)", rpl, s)
-
-def hexify_colors(s):
-    def rpl(m):
-        prefix = m.group(1)
-        r = m.group(2)
-        r = r.replace("g","6")
-        r = r.replace("i","1")
-        r = r.replace("l","1")
-        r = r.replace("o","0")
-        r = r.replace("s","5")
-        r = r.replace("t","7")
-        r = r.replace("z","2")
-        r = r.upper()
-        r = "#" + r
-        return prefix + r
-    return re.sub("(^|\\s)([0-9abcdefgilostz]{3}|[0-9abcdefgilostz]{6}|[0-9abcdefgilostz]{8})(?=$|\\s)", rpl, s)
+        r = ("#" if colors else "0x") + r.upper()
+        return match.group(1) + r
+    if colors:
+        reg = "(^|\\s)([0-9abcdefgilostz]{3}|[0-9abcdefgilostz]{6}|[0-9abcdefgilostz]{8})(?=$|\\s)"
+    else:
+        reg = f"(^|\\s)([0-9abcdefgilostz]{{{min_length},}})(?=$|\\s)"
+    return re.sub(reg, rpl, string)
 
 test = '''
 ## tests
@@ -57,5 +44,5 @@ pragma
 zen
 '''
 
-# print(hexify(test))
 print(hexify(test))
+# print(hexify(test, True))
